b776e6147d472469c810c6fdaabe5d4b5681ad1f
wasuken <wevorence@gmail.com>
2024-06-08 10:22:12 +0900

    first commit

---
+++ Dockerfile.php
@@ -0,0 +1,23 @@
+FROM php:8.3-fpm
+
+RUN apt-get update && apt-get install -y \
+    libicu-dev \
+    libonig-dev \
+    libzip-dev \
+    unzip \
+    && docker-php-ext-configure intl \
+    && docker-php-ext-install intl mbstring pdo_mysql zip mysqli
+
+RUN useradd --shell /bin/bash -u 1000 -o -c "" -m runuser
+RUN mkdir -p /shared/tmp && chown runuser. /shared/ -R
+RUN chown runuser:runuser /var/www/html
+
+WORKDIR /var/www/html
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+USER runuser
+COPY ./php/composer*.json ./
+RUN composer install
+
+CMD ["php", "spark", "serve", "--host", "0.0.0.0"]
