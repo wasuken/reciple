222f7d06ece660052564ed69b3600b3a7a3d2494
wasuken <wevorence@gmail.com>
2024-07-20 10:48:36 +0900

    アップロードファイルにアクセスできるように
    最終的にここらへんはクラウドストレージかファイルストレージかで抽象化し
    たい。
    
    3. レシピ一覧コンポーネントの修正
       - タグリストの表示
    4. 結合確認
    5. レシピ作成ページを実装
    6. auth系のルーティングいいかげんディレクトリきりたい

--- php/app/Config/Routes.php
+++ php/app/Config/Routes.php
@@ -9,6 +9,21 @@ use CodeIgniter\Router\RouteCollection;
 // $routes->get('/', 'Home::index');
 
 $routes->group('api', static function ($routes) {
+    // 静的ファイルのルートを追加
+    $routes->add('uploads/stored/(:any)', function($file) {
+        $path = WRITEPATH . 'uploads/stored/' . $file;
+
+        if (file_exists($path)) {
+            // ファイルのMIMEタイプを取得
+            $mimeType = mime_content_type($path);
+            header("Content-Type: $mimeType");
+            readfile($path);
+            exit;
+        } else {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+    });
+
     $routes->post('login', 'Login::id_password');
     $routes->post('login/google', 'Login::google');
 
